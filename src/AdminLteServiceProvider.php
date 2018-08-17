503da0d8755725bd6a8cf9f46b31949b115643ed
Igor C. de Paula <principe.borodin@gmail.com>
2018-08-17 11:36:52 -0300

    initial

---
+++ src/AdminLteServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: igor
+ * Date: 15/08/18
+ * Time: 16:09
+ */
+
+namespace BrasilPHP\AdminLte;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminLteServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../assets' => public_path(),
+        ], 'adminlte:assets');
+
+        $this->loadRoutesFrom(__DIR__.'/../routes/routes.php');
+        $this->loadViewsFrom(__DIR__.'/../views','adminlte');
+        $this->commands([
+
+        ]);
+    }
+
+    public function register()
+    {
+
+    }
+}
